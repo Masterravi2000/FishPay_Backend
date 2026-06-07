07daa425cf9974826090035d123369683de89dfa
Masterravi2000 <raviqazqwerty123456@gmail.com>
2026-06-08 01:46:29 +0530

    Spring Backend initialised

---
+++ src/main/java/com/fishpay/FishpayApplication.java
@@ -0,0 +1,13 @@
+package com.fishpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fishpay/FishpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
